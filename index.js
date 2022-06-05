4fb947ae897a78f9ad08e85b0983a9f4c87219ca
Budianto IP <budiantoip@gmail.com>
2022-06-05 19:58:29 +0700

    build sort functions

--- index.js
+++ index.js
@@ -6,15 +6,21 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (names, callback) => {
+  return callback(names).map((name, index) => `${index + 1}. ${name}`);
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (array) => {
+  return array.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (array) => {
+  return array.sort().reverse();
+}
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
